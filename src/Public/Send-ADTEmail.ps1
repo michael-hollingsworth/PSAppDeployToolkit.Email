27dbd8302ab8929c7250881133c9abb6ddc34c86
Michael Hollingsworth <michaelhollingsworth1093@gmail.com>
2026-02-06 23:33:43 -0500

    Update default value help messages

--- src/Public/Send-ADTEmail.ps1
+++ src/Public/Send-ADTEmail.ps1
@@ -2,19 +2,19 @@ function Send-Email {
     [CmdletBinding()]
     param (
         [Parameter()]
-        [PSDefaultValue(Help = '(Get-ADTConfig).Email.From')]
+        [PSDefaultValue(Help = '(Get-ADTConfig).Email.Defaults.From')]
         [ValidateNotNullOrEmpty()]
         [System.Net.Mail.MailAddress]$From,
         [Parameter()]
-        [PSDefaultValue(Help = '(Get-ADTConfig).Email.To')]
+        [PSDefaultValue(Help = '(Get-ADTConfig).Email.Defaults.To')]
         [ValidateNotNullOrEmpty()]
         [System.Net.Mail.MailAddress[]]$To,
         [Parameter()]
-        [PSDefaultValue(Help = '(Get-ADTConfig).Email.Cc')]
+        [PSDefaultValue(Help = '(Get-ADTConfig).Email.Defaults.Cc')]
         [ValidateNotNullOrEmpty()]
         [System.Net.Mail.MailAddress[]]$Cc,
         [Parameter()]
-        [PSDefaultValue(Help = '(Get-ADTConfig).Email.Bcc')]
+        [PSDefaultValue(Help = '(Get-ADTConfig).Email.Defaults.Bcc')]
         [ValidateNotNullOrEmpty()]
         [System.Net.Mail.MailAddress[]]$Bcc,
         [Parameter()]
@@ -29,20 +29,20 @@ function Send-Email {
         [Parameter()]
         [Switch]$IncludeLogs,
         [Parameter()]
-        [PSDefaultValue(Help = '(Get-ADTConfig).Email.Priority')]
+        [PSDefaultValue(Help = '(Get-ADTConfig).Email.Defaults.Priority')]
         [System.Net.Mail.MailPriority]$Priority,
         [Parameter()]
         [ValidateNotNullOrEmpty()]
         [String]$SmtpServer,
         [Parameter()]
-        [PSDefaultValue(Help = '(Get-ADTConfig).Email.Port')]
+        [PSDefaultValue(Help = '(Get-ADTConfig).Email.Defaults.SmtpClient.Port')]
         [ValidateRange(1, [Int32]::MaxValue)]
         [Int32]$Port = 25,
         [Parameter()]
-        [PSDefaultValue(Help = '(Get-ADTConfig).Email.UseDefaultCredentials')]
+        [PSDefaultValue(Help = '(Get-ADTConfig).Email.Defaults.SmtpClient.UseDefaultCredentials')]
         [Switch]$UseDefaultCredentials,
         [Parameter()]
-        [PSDefaultValue(Help = '(Get-ADTConfig).Email.EnableSsl')]
+        [PSDefaultValue(Help = '(Get-ADTConfig).Email.Defaults.SmtpClient.EnableSsl')]
         [Alias('UseSsl')]
         [Switch]$EnableSsl,
         [Switch]$Defer
