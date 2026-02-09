1b9ab64a4a7627078a759dde77182af7245d1a65
Michael Hollingsworth <michaelhollingsworth1093@gmail.com>
2026-02-09 00:39:57 -0500

    Update import system and use command table to pass private functions to `Add-ADTModuleCallback`

--- src/ImportsFirst.ps1
+++ src/ImportsFirst.ps1
@@ -1,3 +1,31 @@
-$ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
-$ProgressPreference = [System.Management.Automation.ActionPreference]::SilentlyContinue
-Set-StrictMode -Version 1.0
+# Throw if this psm1 file isn't being imported via our manifest.
+if (-not ([System.Environment]::StackTrace.Split("`n") -like '*Microsoft.PowerShell.Commands.ModuleCmdletBase.LoadModuleManifest(*')) {
+    throw [System.Management.Automation.ErrorRecord]::new(
+        [System.InvalidOperationException]::new('This module must be imported via its .psd1 file, which is recommended for all modules that supply a .psd1 file.'),
+        'ModuleImportError',
+        [System.Management.Automation.ErrorCategory]::InvalidOperation,
+        $MyInvocation.MyCommand.ScriptBlock.Module
+    )
+}
+
+# Rethrowing caught exceptions makes the error output from Import-Module look better.
+try {
+    # Set up lookup table for all cmdlets used within module, using PSAppDeployToolkit's as a basis.
+    $CommandTable = [System.Collections.Generic.Dictionary[System.String, System.Management.Automation.CommandInfo]](& (& 'Microsoft.PowerShell.Core\Get-Command' -Name Get-ADTCommandTable -FullyQualifiedModule @{ ModuleName = 'PSAppDeployToolkit'; Guid = '8c3c366b-8606-4576-9f2d-4051144f7ca2'; ModuleVersion = '4.0.6' }))
+
+    # Set required variables to ensure module functionality.
+    New-Variable -Name ErrorActionPreference -Value ([System.Management.Automation.ActionPreference]::Stop) -Option Constant -Force
+    New-Variable -Name InformationPreference -Value ([System.Management.Automation.ActionPreference]::Continue) -Option Constant -Force
+    New-Variable -Name ProgressPreference -Value ([System.Management.Automation.ActionPreference]::SilentlyContinue) -Option Constant -Force
+
+    # Ensure module operates under the strictest of conditions.
+    Set-StrictMode -Version 3
+
+    # Remove any previous functions that may have been defined.
+    if ($MyInvocation.MyCommand.Name.Equals('PSAppDeployToolkit.Email.psm1')) {
+        New-Variable -Name FunctionPaths -Option Constant -Value ($MyInvocation.MyCommand.ScriptBlock.Ast.EndBlock.Statements | & { process { if ($_ -is [System.Management.Automation.Language.FunctionDefinitionAst]) { return "Microsoft.PowerShell.Core\Function::$($_.Name)" } } })
+        Remove-Item -LiteralPath $FunctionPaths -Force -ErrorAction Ignore
+    }
+} catch {
+    throw
+}
--- src/ImportsLast.ps1
+++ src/ImportsLast.ps1
@@ -1,24 +1,35 @@
-Add-ADTModuleCallback -Hookpoint OnStart -Callback (Get-Command -Name 'Confirm-ADTEmailConfig')
-Add-ADTModuleCallback -Hookpoint PostOpen -Callback (Get-Command -Name 'Initialize-ADTDeferredEmailsProperty')
-Add-ADTModuleCallback -Hookpoint PostOpen -Callback (Get-Command -Name 'Initialize-ADTAdditionalLogFilesProperty')
-Add-ADTModuleCallback -Hookpoint PreClose -Callback (Get-Command -Name 'Send-ADTDeferredEmails')
-Add-ADTModuleCallback -Hookpoint PreClose -Callback (Get-Command -Name 'Send-ADTEmailOnErrorExit')
+try {
+    # Set all functions as read-only, export all public definitions and finalise the CommandTable.
+    Set-Item -LiteralPath $FunctionPaths -Options ReadOnly
+    Get-Item -LiteralPath $FunctionPaths | & { process { $CommandTable.Add($_.Name, $_) } }
+    New-Variable -Name CommandTable -Value ([System.Collections.ObjectModel.ReadOnlyDictionary[System.String, System.Management.Automation.CommandInfo]]::new($CommandTable)) -Option Constant -Force -Confirm:$false
+    Export-ModuleMember -Function $Module.Manifest.FunctionsToExport
 
-if (Test-ADTModuleInitialized) {
-    Confirm-ADTEmailConfig
-}
+    Add-ADTModuleCallback -Hookpoint OnStart -Callback $CommandTable.'Confirm-ADTEmailConfig'
+    Add-ADTModuleCallback -Hookpoint PostOpen -Callback $CommandTable.'Initialize-ADTDeferredEmailsProperty'
+    Add-ADTModuleCallback -Hookpoint PostOpen -Callback $CommandTable.'Initialize-ADTAdditionalLogFilesProperty'
+    Add-ADTModuleCallback -Hookpoint PreClose -Callback $CommandTable.'Send-ADTDeferredEmails'
+    Add-ADTModuleCallback -Hookpoint PreClose -Callback $CommandTable.'Send-ADTEmailOnErrorExit'
 
-# Initialize session properties if a session already exists
-if (Test-ADTSessionActive) {
-    $adtSession = Get-ADTSession
-
-    if (-not $adtSession.PSObject.Properties.ContainsKey('DeferredEmails')) {
-        Initialize-ADTDeferredEmailsProperty
+    if (Test-ADTModuleInitialized) {
+        Confirm-ADTEmailConfig
     }
 
-    if (-not $adtSession.PSObject.Properties.ContainsKey('AdditionalLogFiles')) {
-        Initialize-ADTAdditionalLogFilesProperty
+    # Initialize session properties if a session already exists
+    if (Test-ADTSessionActive) {
+        $adtSession = Get-ADTSession
+    
+        if (-not $adtSession.PSObject.Properties.ContainsKey('DeferredEmails')) {
+            Initialize-ADTDeferredEmailsProperty
+        }
+    
+        if (-not $adtSession.PSObject.Properties.ContainsKey('AdditionalLogFiles')) {
+            Initialize-ADTAdditionalLogFilesProperty
+        }
     }
-}
 
-Write-ADTLogEntry -Message "Module [$($MyInvocation.MyCommand.ScriptBlock.Module.Name)] imported successfully." -ScriptSection Initialization
+    # Announce successful importation of module.
+    Write-ADTLogEntry -Message "Module [$($MyInvocation.MyCommand.ScriptBlock.Module.Name)] imported successfully." -ScriptSection Initialization
+} catch {
+    throw
+}
